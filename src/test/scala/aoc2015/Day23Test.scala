956a4ecf599031be0fbfc4cd206645a49e261658
merens <26874661+merens@users.noreply.github.com>
2022-10-25 19:11:01 +0200

    Fix 2015 day 23 test

--- src/test/scala/aoc2015/Day23Test.scala
+++ src/test/scala/aoc2015/Day23Test.scala
@@ -5,44 +5,44 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class Day23Test extends AnyFunSuite {
   test("test instruction increment") {
-    val s = State(0, 0, 0)
+    val s = State(Map("a" -> 0, "b" -> 0), 0)
     val instruction = "inc a"
-    val expected = State(1, 0, 1)
+    val expected = State(Map("a" -> 1, "b" -> 0), 1)
     val actual = logic(s, instruction)
 
     assert(actual == expected)
   }
 
   test("test instruction triple") {
-    val s = State(1, 2, 3)
+    val s = State(Map("a" -> 1, "b" -> 2), 3)
     val instruction = "tpl b"
-    val expected = State(1, 6, 4)
+    val expected = State(Map("a" -> 1, "b" -> 6), 4)
     val actual = logic(s, instruction)
 
     assert(actual == expected)
   }
   test("test instruction half") {
-    val s = State(1, 2, 3)
+    val s = State(Map("a" -> 1, "b" -> 2), 3)
     val instruction = "hlf b"
-    val expected = State(1, 1, 4)
+    val expected = State(Map("a" -> 1, "b" -> 1), 4)
     val actual = logic(s, instruction)
 
     assert(actual == expected)
   }
 
   test("test instruction forward jump") {
-    val s = State(1, 2, 3)
+    val s = State(Map("a" -> 1, "b" -> 2), 3)
     val instruction = "jmp +5"
-    val expected = State(1, 2, 8)
+    val expected = State(Map("a" -> 1, "b" -> 2), 8)
     val actual = logic(s, instruction)
 
     assert(actual == expected)
   }
 
   test("test instruction backwards jump") {
-    val s = State(1, 2, 5)
+    val s = State(Map("a" -> 1, "b" -> 2), 5)
     val instruction = "jmp -4"
-    val expected = State(1, 2, 1)
+    val expected = State(Map("a" -> 1, "b" -> 2), 1)
     val actual = logic(s, instruction)
 
     assert(actual == expected)
